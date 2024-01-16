b3d94046baa4d9905886baf1239a3c9700cbac00
Shyamal Das <shyamald934@gmail.com>
2024-01-16 18:27:29 +0530

    Initial Commit !

---
+++ client/src/conf/conf.js
@@ -0,0 +1,6 @@
+// environment variables
+const conf={
+    userKey: String(import.meta.env.VITE_LOCALHOST_KEY),
+}
+
+export default conf;
---
+++ server/index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+
+app.use(cors());
+app.use(express.json());
+
+// db connection
+main().catch(err=> console.log(err));
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('Database connected')
+}
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
